bbc763a8351703ee04e4c4692833e71cfc4e2a33
Garrett van Wageningen <ghvanw283@gmail.com>
2023-08-25 11:56:30 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ pub trait Parser {
 
     fn parse<'a>(&self, input: &'a str) -> Option<(Self::Out, &'a str)>;
 
-    fn map<B>(&self, transform: Box<dyn Fn(Self::Out) -> B>) -> Map<Self, B> where Self: Sized {
+    // fn map<B>(self, transform: Box<dyn Fn(Self::Out) -> B>) -> Map<Self, B> where Self: Sized {
+    fn map<B, F: Fn(Self::Out) -> B>(self, transform: F) -> Map<Self, F> where Self: Sized {
         Map::new(transform, self)
     }
 }
@@ -49,20 +50,21 @@ impl<A> Parser for Zero<A> {
 }
 
 
-pub struct Satisfy {
-    predicate: Box<dyn Fn(char) -> bool>
+pub struct Satisfy<P> {
+    // predicate: Box<dyn Fn(char) -> bool>
+    predicate: P
 }
 
 
-impl Satisfy {
-    pub fn new(predicate: Box<dyn Fn(char) -> bool>) -> Self {
+impl<P> Satisfy<P> {
+    pub fn new(predicate: P) -> Self {
         Self {
             predicate: predicate
         }
     }
 }
 
-impl Parser for Satisfy {
+impl<P: Fn(char) -> bool> Parser for Satisfy<P> {
     type Out = char;
 
     fn parse<'a>(&self, input: &'a str) -> Option<(Self::Out, &'a str)> {
@@ -76,14 +78,14 @@ impl Parser for Satisfy {
     }
 }
 
-pub struct Map<P: Parser, B> {
-    transform: Box<dyn Fn(<P as Parser>::Out) -> B>,
+pub struct Map<P: Parser, F> {
+    transform: F,
     parser: P
 }
 
 
-impl<P: Parser, B> Map<P, B> {
-    pub fn new(transform: Box<dyn Fn(<P as Parser>::Out) -> B>, parser: P) -> Self {
+impl<P: Parser, F> Map<P, F> {
+    pub fn new(transform: F, parser: P) -> Self {
         Self {
             transform: transform,
             parser: parser
@@ -91,7 +93,7 @@ impl<P: Parser, B> Map<P, B> {
     }
 }
 
-impl<P: Parser, B> Parser for Map<P, B> {
+impl<P: Parser, B, F: Fn(<P as Parser>::Out) -> B> Parser for Map<P, F> {
     type Out = B;
 
     fn parse<'a>(&self, input: &'a str) -> Option<(Self::Out, &'a str)> {
@@ -103,12 +105,79 @@ impl<P: Parser, B> Parser for Map<P, B> {
 
 
 
+pub struct Char {
+    it: char
+}
+
+impl Char {
+    pub fn new(it: char) -> Char {
+        Self { it: it }
+    }
+}
+
+impl Parser for Char {
+    type Out = char;
+
+    fn parse<'a>(&self, input: &'a str) -> Option<(Self::Out, &'a str)> {
+        Satisfy::new(|item| item == self.it).parse(input)
+    }
+}
+
+pub fn whitespace() -> Satisfy<fn(char) -> bool> {
+    Satisfy { predicate: char::is_whitespace }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::assert_eq;
+
     use super::*;
 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn hello_world_satisfies_char_h() {
+        let it = "hello world";
+
+        let (result, rest) = Char::new('h').parse(it).unwrap();
+
+
+        assert_eq!(result, 'h');
+        assert_eq!(rest, "ello world");
+    }
+
+
+    #[test]
+    fn hello_world_h_mapped() {
+        let it = "hello world";
+
+
+        let (result, rest): (char, &str) = 
+            Char::new('h')
+                .map(|item| item.to_ascii_uppercase())
+                .parse(it)
+                .unwrap();
+
+
+        assert_eq!(result, 'H');
+        assert_eq!(rest, "ello world");
+    }
+
+    #[test]
+    fn whitespace_test() {
+        let it = " world";
+
+
+        let (result, rest): (char, &str) = 
+            whitespace()
+                .parse(it)
+                .unwrap();
+
+
+        assert_eq!(result, ' ');
+        assert_eq!(rest, "world");
     }
 }
